818e8d0bd33363a6bc809468ba211fd52b8b202c
Baharvandiaboalfazl <109616285+Baharvandiaboalfazl@users.noreply.github.com>
2025-08-21 02:02:08 +0330

    Update RobotLoader.jsx

--- client/src/components/threejs/RobotLoader.jsx
+++ client/src/components/threejs/RobotLoader.jsx
@@ -6,7 +6,7 @@ import { BackgroundBeamsWithCollision } from "../ui/BackgroundBeams";
 
 function Model(props) {
   const group = useRef();
-  const { scene, animations } = useGLTF(`tech-log/robot.glb`);
+  const { scene, animations } = useGLTF(`robot.glb`);
   const { actions } = useAnimations(animations, group);
 
   useEffect(() => {
