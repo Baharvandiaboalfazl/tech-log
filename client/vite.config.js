13b10711daebea9f24ef566d3f29896410f14fe7
Baharvandiaboalfazl <baharvandiaboalfazl@gmail.com>
2025-08-20 18:03:52 +0330

    deploy change

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000",
+  //       secure: false,
+  //     },
+  //   },
+  // },
   base: "/tech-log/",
   plugins: [react(), tailwindcss()],
 });
