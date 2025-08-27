3bf30fefac03d0e79af26084da33907e8a43afab
Baharvandiaboalfazl <109616285+Baharvandiaboalfazl@users.noreply.github.com>
2025-08-27 12:04:02 +0330

    Update verifyUser.js

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -3,14 +3,16 @@ import { errorHandler } from "./errorHandler.js";
 
 export const verifyToken = (req, res, next) => {
   try {
-    const token = req.cookies.access_token;
-
-    if (!token) {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return next(errorHandler(401, "دسترسی غیرمجاز", "لطفا ابتدا وارد شوید."));
     }
 
+    const token = authHeader.split(' ')[1];
+
     const userPayload = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = userPayload;
+    req.user = userPayload; 
     next();
   } catch (error) {
     return next(errorHandler(403, "دسترسی ممنوع", "توکن نامعتبر است."));
